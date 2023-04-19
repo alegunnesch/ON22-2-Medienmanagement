b9227171d945fe4f335f568defef8ce303890dd6
alegunnesch <alegunnesch.git@gmail.com>
2023-04-19 17:12:13 +0200

    Test von einem Comment und Branch1

---
+++ demo/src/main/java/on22/medienprojekt/PrimaryController.java
@@ -0,0 +1,14 @@
+package on22.medienprojekt;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+
+    // Das ist ein Comment zum Testen
+}
