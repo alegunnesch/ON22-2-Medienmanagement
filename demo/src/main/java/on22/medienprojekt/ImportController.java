bcfc97980be71de9dd6b6960436a95e1468c0afb
ON22JB <jacqbohn@aol.com>
2023-04-22 15:12:57 +0200

    button added actions

--- demo/src/main/java/on22/medienprojekt/ImportController.java
+++ demo/src/main/java/on22/medienprojekt/ImportController.java
@@ -3,7 +3,7 @@ package on22.medienprojekt;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-public class SecondaryController {
+public class ImportController {
 
     @FXML
     private void switchToPrimary() throws IOException {
--- demo/src/main/java/on22/medienprojekt/PrimaryController.java
+++ demo/src/main/java/on22/medienprojekt/PrimaryController.java
@@ -6,7 +6,19 @@ import javafx.fxml.FXML;
 public class PrimaryController {
 
     @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+    @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+    @FXML
+    private void switchToTagliste() throws IOException {
+        App.setRoot("tagliste");
+    }
+    @FXML
+    private void switchToImport() throws IOException {
+        App.setRoot("import");
     }
 }
