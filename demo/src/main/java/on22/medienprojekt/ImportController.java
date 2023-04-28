5b6d97336258a7ad08aeb8cc9041ccb063729ad8
ON22JB <jacqbohn@aol.com>
2023-04-28 17:34:14 +0200

    Option to select file type and add file to lv

--- demo/src/main/java/on22/medienprojekt/ImportController.java
+++ demo/src/main/java/on22/medienprojekt/ImportController.java
@@ -2,8 +2,7 @@ package on22.medienprojekt;
 
 import java.io.File;
 import java.io.IOException;
-
-
+import java.util.List;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -11,6 +10,7 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class ImportController {
@@ -42,8 +42,10 @@ public class ImportController {
         private TextField textFieldBrowse;
 
         @FXML
-        private ListView<?> lvFiles;
+        private ListView<File> lvFiles;
 
+
+        final FileChooser fc = new FileChooser();
     
         @FXML
         private void handleBrowseButton() throws IOException {
@@ -54,5 +56,31 @@ public class ImportController {
                 System.out.println("Path: " + selectedDirectory.getAbsolutePath());
                 textFieldBrowse.setText(selectedDirectory.getAbsolutePath());
             }
+
+
+            fc.setTitle("Wähle Dateien aus");
+            fc.setInitialDirectory(new File(System.getProperty("user.home")));
+            fc.getExtensionFilters().clear();
+            fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("All Files","*.*"));
+            fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("PDF","*.pdf"));
+            fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("JPG","*.jpg"));
+            fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("PNG","*.png"));
+            fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("HTML","*.html"));
+            fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("CSS","*.css"));
+            fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("MP3","*.mp3"));
+            fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("MP4","*.mp4"));
+            fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("TIFF","*.tiff"));
+            fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("MKV","*.mkv"));
+            List<File> files = fc.showOpenMultipleDialog(null);
+
+            for (int i = 0; i < files.size(); i++){
+                if (files != null) {
+                    System.out.println("test");
+                    lvFiles.getItems().add(files.get(i));
+                }
+                //textFieldBrowse.appendText(files.get(i).getAbsolutePath()+ "\n");
+            }
+           
+
     }
 }
