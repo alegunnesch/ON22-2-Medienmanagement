519f5fb24401145d40b9ba2b1322c5252cbd6a74
Viviienne <120227203+Viviienne@users.noreply.github.com>
2023-05-08 18:58:33 +0100

    TreeView

--- demo/src/main/java/on22/medienprojekt/ManagementController.java
+++ demo/src/main/java/on22/medienprojekt/ManagementController.java
@@ -1,9 +1,15 @@
 package on22.medienprojekt;
 
 import java.io.IOException;
-import javafx.fxml.FXML;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ManagementController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+
+public class ManagementController implements Initializable {
 
     @FXML
     private void switchToStart() throws IOException {
@@ -21,4 +27,40 @@ public class ManagementController {
     private void switchToImport() throws IOException {
         App.setRoot("import");
     }
-}
+    @FXML
+    private TreeView treeView;
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        
+       TreeItem<String> rootItem = new TreeItem<>("Root");
+
+       TreeItem<String> branchItem1 = new TreeItem<>("Folder 1");
+       TreeItem<String> branchItem2 = new TreeItem<>("file.css");
+       TreeItem<String> branchItem3 = new TreeItem<>("file.js");
+
+       TreeItem<String> twigItem1 = new TreeItem<>("Folder 2");
+       TreeItem<String> twigItem2 = new TreeItem<>("file.html");
+       
+       TreeItem<String> leafItem1 = new TreeItem<>("file1.xml");
+       TreeItem<String> leafItem2 = new TreeItem<>("file2.xml");
+       TreeItem<String> leafItem3 = new TreeItem<>("file3.xml");
+       
+       
+       twigItem1.getChildren().addAll(leafItem1,leafItem2, leafItem3);
+
+       branchItem1.getChildren().addAll(twigItem1,twigItem2);
+           
+       rootItem.getChildren().addAll(branchItem1,branchItem2,branchItem3);
+
+       treeView.setRoot(rootItem);
+    }
+
+    public void selectItem() {
+        TreeItem<String> selectedItem = (TreeItem<String>) treeView.getSelectionModel().getSelectedItem();
+        
+        if(selectedItem != null) {
+            System.out.println(selectedItem.getValue());
+        }
+     }
+    }     
