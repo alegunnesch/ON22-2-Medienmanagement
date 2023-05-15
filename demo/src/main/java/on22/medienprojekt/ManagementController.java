0594e93430530704ab7e338410b751261f8e7576
Nilsiii <119663254+Nilsiii@users.noreply.github.com>
2023-05-15 23:30:01 +0200

    added some stuff

--- demo/src/main/java/on22/medienprojekt/ManagementController.java
+++ demo/src/main/java/on22/medienprojekt/ManagementController.java
@@ -10,8 +10,19 @@ import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.layout.StackPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 import javafx.scene.control.ListView;
+
+//auslesen der .daf
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ManagementController implements Initializable {
 
@@ -30,10 +41,26 @@ public class ManagementController implements Initializable {
         App.setRoot("taglist");
     }
     
+    /**
+     * @throws IOException
+     */
     @FXML
     private void switchToImport() throws IOException {
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Import DAF File");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("DAF Files", "*.daf"));
+        Stage stage = (Stage) treeView.getScene().getWindow();
+        File selectedFile = fileChooser.showOpenDialog(stage);
+        if (selectedFile != null) {
+            String filePath = selectedFile.getAbsolutePath();
+            List<String> fileContent = readDafFile(filePath);
+            fileList.setItems(FXCollections.observableArrayList(fileContent));
+
+
         App.setRoot("import");
     }
+        }
     
     @FXML
     private ListView<String> fileList;
@@ -44,6 +71,8 @@ public class ManagementController implements Initializable {
 
     @FXML
     private TreeView treeView;
+    
+    
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
@@ -78,4 +107,41 @@ public class ManagementController implements Initializable {
             System.out.println(selectedItem.getValue());
         }
      }
-    }     
+
+     //auslesen der .daf
+
+ 
+    public static class ReadDafFile {
+         public static void main(String[] args) {
+        String filePath = "C:\\Users\\WN00812443\\Desktop\\ProjektLastTry\\ON22-2-Medienmanagement\\IvFiles";
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            String line = reader.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    }
+
+
+//für die ausgabe im list view
+
+public static List<String> readDafFile(String filePath) throws IOException {
+    List<String> lines = new ArrayList<>();
+    BufferedReader reader = new BufferedReader(new FileReader(filePath));
+    String line = reader.readLine();
+    while (line != null) {
+        lines.add(line);
+        line = reader.readLine();
+    }
+    reader.close();
+    return lines;
+}
+
+
+}
