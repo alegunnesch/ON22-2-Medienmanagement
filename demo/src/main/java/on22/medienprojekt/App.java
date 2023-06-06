30bf35819b7526a313b6418222202da800d0a7e9
Viviienne <120227203+Viviienne@users.noreply.github.com>
2023-06-06 22:16:30 +0200

    resize fix

--- demo/src/main/java/on22/medienprojekt/App.java
+++ demo/src/main/java/on22/medienprojekt/App.java
@@ -18,7 +18,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("start"), 1280, 720);
-        stage. setResizable(false);
+        stage. setResizable(true);
         stage.setScene(scene);
         stage.show();
     }
